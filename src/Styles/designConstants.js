c0dc501d9cc363c13241461cdb7051eee7f6f358
Suzanne Cabral <suzannecabral@users.noreply.github.com>
2021-05-03 15:04:57 -0700

    Added text colors to designConstants

--- src/Styles/designConstants.js
+++ src/Styles/designConstants.js
@@ -1,18 +1,26 @@
 export const colors = {
 
     /* Material: Pink 700 */
+    // deep raspberry pink
     accentMain:'#c01e5b',
     // favicon sampled: #c01e5b
     // material pink 700: #c2185b
-
     accentBright:'#fa5788',
     accentDark:'#8c0032',
 
+    accentMainText:'#ffffff',
+    accentBrightText:'#000000',
+    accentDarkText:'#ffffff',
+
     /* Material: Blue 50 */
+    // White/gray with hint of blue
     neutralWhite:'#ffffff',
     neutralTint:'#e3f2fd',
     neutralGray:'#b1bfca',
 
+    neutralWhiteText:'#000000',
+    neutralTintText:'#000000',
+    neutralDarkText:'#000000',
 }
 
 export default colors;
