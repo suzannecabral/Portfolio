b13a5f3e1f6e844c139c524b3818ec1f262edaa6
Suzanne Cabral <suzannecabral@users.noreply.github.com>
2021-05-03 14:52:37 -0700

    Added designConstants to store css theme values

---
+++ src/Styles/designConstants.js
@@ -0,0 +1,18 @@
+export const colors = {
+
+    /* Material: Pink 700 */
+    accentMain:'#c01e5b',
+    // favicon sampled: #c01e5b
+    // material pink 700: #c2185b
+
+    accentBright:'#fa5788',
+    accentDark:'#8c0032',
+
+    /* Material: Blue 50 */
+    neutralWhite:'#ffffff',
+    neutralTint:'#e3f2fd',
+    neutralGray:'#b1bfca',
+
+}
+
+export default colors;
