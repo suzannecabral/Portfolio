c5637d1d7c406e9b411d3d734ac91b0adfa1208f
Suzanne Cabral <suzannecabral@users.noreply.github.com>
2021-05-03 15:14:28 -0700

    Added sidebar alignment rules

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -3,7 +3,15 @@ import styled from 'styled-components';
 import colors from '../Styles/designConstants';
 
 const StyledSidebar = styled.div`
+    position:fixed;
+    top:0;
+    left:0;
+    bottom:0;
+    width:16rem;
+    padding:2rem 1rem;
+    text-align:left;
     background-color:${colors.accentMain};
+    color:${colors.accentMainText};
 `;
 
 const Sidebar = props => {
